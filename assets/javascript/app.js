719a8fca10e9180f0905343300e8df6846d7174a
Diana <dianajulaton@gmail.com>
2019-01-21 13:14:52 -0800

    added instagram acct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,17 +9,20 @@ $(document).ready(function() {
     $(window).scroll(function() {
       if ($(window).scrollTop() > 56) {
         $(".navbar").addClass("bg-dark");
+        $(".nav-link").addClass("text-light bg-dark");
       } else {
         $(".navbar").removeClass("bg-dark");
-      }
+        $(".nav-link").removeClass("text-light bg-dark");      }
     });
     // If Mobile, add background color when toggler is clicked
     $(".navbar-toggler").click(function() {
       if (!$(".navbar-collapse").hasClass("show")) {
         $(".navbar").addClass("bg-dark");
+        $(".nav-link").addClass("text-light bg-dark");
       } else {
         if ($(window).scrollTop() < 56) {
           $(".navbar").removeClass("bg-dark");
+          $(".nav-link").removeClass("text-light bg-dark");
         } else {
         }
       }
